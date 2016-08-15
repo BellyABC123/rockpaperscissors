cdf0978c750249c2c2aa89ddc669167e2da6375e
Rasheed Wihaib <rasheed.wihaib@shazam.com>
2016-08-15 15:48:59 +0100

    Initial Commit

---
+++ MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by rasheed.wihaib on 15/08/2016.
+//  Copyright © 2016 BigKahuna. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
